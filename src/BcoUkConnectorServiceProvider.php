4716e22bbb3571c29c91de271590851522f5d147
appdev112 <refo@bwisemedia.com>
2025-11-10 14:20:09 +0800

    register new DB connection

--- src/BcoUkConnectorServiceProvider.php
+++ src/BcoUkConnectorServiceProvider.php
@@ -20,4 +20,47 @@ class BcoUkConnectorServiceProvider extends PackageServiceProvider
             ->hasViews()
             ->hasMigration('create_migration_table_name_table');
     }
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $this->registerDatabaseConnection();
+    }
+
+    protected function registerDatabaseConnection(): void
+    {
+        $config = config('b-co-uk-connector');
+
+        $connectionConfig = array_merge(
+            $this->getDriverDefaults($config['db_connection'] ?? 'pgsql'),
+            [
+                'driver' => $config['db_connection'] ?? 'pgsql',
+                'host' => $config['db_host'] ?? 'localhost',
+                'port' => $config['db_port'] ?? '5432',
+                'database' => $config['db_name'] ?? 'b_co_uk',
+                'username' => $config['db_username'] ?? 'root',
+                'password' => $config['db_password'] ?? 'root',
+            ]
+        );
+
+        config(['database.connections.b_co_uk' => $connectionConfig]);
+    }
+
+    protected function getDriverDefaults(string $driver): array
+    {
+        return match ($driver) {
+            'pgsql' => [
+                'charset' => 'utf8',
+                'search_path' => 'public',
+                'sslmode' => 'prefer',
+                'prefix' => '',
+                'prefix_indexes' => true,
+            ],
+            default => [
+                'prefix' => '',
+                'prefix_indexes' => true,
+            ],
+        };
+    }
 }
